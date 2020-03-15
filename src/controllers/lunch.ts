65504303f1852a36318047718c106cc3b231c90c
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-03-14 22:53:32 -0600

    Testing request

--- src/controllers/lunch.ts
+++ src/controllers/lunch.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
-
+import { getRepository } from 'typeorm';
+import Lunch from '../entities/Lunch';
 class LunchController {
-    public static getRandomLunch = (req: Request, res: Response) => {
-        res.send("This is your random lunch");
+    public static getRandomLunch = async (req: Request, res: Response) => {
+
+        const lunchRepository = getRepository(Lunch);
+        try {
+            const lunch = await lunchRepository.createQueryBuilder('lunch').where('lunch.id=1').getOne();
+            if (lunch) res.send(lunch);
+            else res.status(404);
+        } catch (error) {
+            res.send("error" + error);
+        }
+
     }
 }
 export default LunchController;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import router from "./routes";
 import cors from 'cors';
 import { createConnection } from "typeorm";
 import config from './helpers/pgconnection';
-
+import 'reflect-metadata';
 
 const PORT: any = process.env.PORT || 4000;
 
