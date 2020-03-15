d9fccfc55df6b90776e12ada91fd5a40daea7fdc
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-03-14 22:16:52 -0600

    Entity and connection

--- db/db.sql
+++ db/db.sql
@@ -3,8 +3,13 @@ CREATE DATABASE lunchdb;
 CREATE TABLE lunchs(
     id SERIAL PRIMARY KEY,
     plate_name VARCHAR(255),
+    plate_description TEXT,
     restaurant_name VARCHAR(255),
     category VARCHAR(255),
     price float,
-    plate_image VARCHAR(MAX)
+    plate_image TEXT
 )
+
+INSERT INTO lunchs (plate_name, plate_description, restaurant_name, category, price, plate_image) VALUES('Boneless', 'Chicken Boneless','Dennys', 'fast-food', 129, 'https://www.dennys.hn/Imagenes/CATEGORIAS/ENTRADAS/D3203.jpg');
+INSERT INTO lunchs (plate_name, plate_description, restaurant_name, category, price, plate_image) VALUES('THE GRAND SLAM', 'Eggs, hotcakes and bacon', 'Dennys', 'breaskfast', 112, 'https://www.dennys.hn/Imagenes/CATEGORIAS/DESAYUNO/D0017.png');
+INSERT INTO lunchs (plate_name, plate_description, restaurant_name, category, price, plate_image) VALUES('CHICKEN AND SHRIMP PASTA', 'Pasta Alfred sauce', 'Dennys', 'pasta', 112, 'https://www.dennys.hn/Imagenes/CATEGORIAS/DESAYUNO/D0017.png');
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import router from "./routes";
 import cors from 'cors';
+import { createConnection } from "typeorm";
+import config from './helpers/pgconnection';
 const app: Application = express();
 
 app.use(express.json());
@@ -8,5 +10,10 @@ app.use(router);
 app.use(cors());
 
 const PORT: any = process.env.PORT || 4000;
-
+try {
+     createConnection(config);
+  } catch (error) {
+    console.log('Error while connecting to the database', error);
+    
+  }
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
